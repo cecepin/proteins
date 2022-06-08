44a8b8d9a5e5237cfbcb891621266b7bfe2d0251
Chris Cepin <cecepin@ncsu.edu>
2022-06-08 16:00:23 +0000

    Update: added additional documentation

--- middle.sh
+++ middle.sh
@@ -1,2 +1,6 @@
 # This script will output the middle portion of a file
+# usage: middle.sh [name of the file] [start number of entries] [end number of entries]
+# example: middle.sh octane 15 3
+
+
 head -n "$2" "$1" | tail -n "$3"
